700a520559a707507a45225e93ca0b4e35b594ee
EtlcT <agathe.badetti@epfedu.fr>
2023-09-19 17:56:09 +0200

    adding GA tags

---
+++ Discovery.md
@@ -0,0 +1,6 @@
+---
+app_name: dig-traces01
+tagline: first lab of digital traces. simple flask app deployed on deta
+---
+
+
--- app.py
+++ app.py
@@ -5,4 +5,16 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    prefix_google = """
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-QFMSBHD3XT"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-QFMSBHD3XT');
+</script>
+"""
+
+    return prefix_google + "<p>Hello, World!</p>"

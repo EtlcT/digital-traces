721a2aac450e567a14f6dbefc29863cf22df18a3
EtlcT <agathe.badetti@epfedu.fr>
2023-10-04 19:56:54 +0200

    adding page /logger/ to our flask wep app to generate logs and view them in deta space ;\nAdding a textarea in a form on the primary page now rendered with html template

--- app.py
+++ app.py
@@ -1,9 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template
+from logging.config import dictConfig
 
+dictConfig({
+    'version': 1,
+    'formatters': {'default': {
+        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+    }},
+    'handlers': {'wsgi': {
+        'class': 'logging.StreamHandler',
+        'stream': 'ext://flask.logging.wsgi_errors_stream',
+        'formatter': 'default'
+    }},
+    'root': {
+        'level': 'INFO',
+        'handlers': ['wsgi']
+    }
+})
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=['GET','POST'])
 def hello_world():
     prefix_google = """
 <!-- Google tag (gtag.js) -->
@@ -17,4 +33,11 @@ def hello_world():
 </script>
 """
 
-    return prefix_google + "<p>Hello, World!</p>"
+    return  prefix_google + render_template('index.html')
+
+@app.route('/logger/')
+def logger():
+  app.logger.info('This is an info message to test logs')
+
+  return '<p>Log page</p>'
+

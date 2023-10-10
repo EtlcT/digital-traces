9a8d54a5a425164cf634e253b23255f7d89aebf9
EtlcT <agathe.badetti@epfedu.fr>
2023-10-10 14:11:31 +0200

    Manipulate cookies with python

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from logging.config import dictConfig
+import requests
 
 dictConfig({
     'version': 1,
@@ -19,8 +20,10 @@ dictConfig({
 
 app = Flask(__name__)
 
+
 @app.route("/", methods=['GET','POST'])
 def hello_world():
+    
     prefix_google = """
 <!-- Google tag (gtag.js) -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-QFMSBHD3XT"></script>
@@ -32,8 +35,7 @@ def hello_world():
   gtag('config', 'G-QFMSBHD3XT');
 </script>
 """
-
-    return  prefix_google + render_template('index.html')
+    return  prefix_google + render_template('index.html', cookies=None, response_text=None, status_code=None)
 
 @app.route('/logger/')
 def logger():
@@ -41,3 +43,29 @@ def logger():
 
   return '<p>Log page</p>'
 
+@app.route('/google_request', methods=['GET','POST'])
+def google_request():
+    try:
+        req = requests.get("https://www.google.com/")
+        if req.status_code == 200 :
+            google_cookies = req.cookies.get_dict()
+            return render_template('index.html', cookies=google_cookies, status_code=None, response_text=None)
+        else:
+            app.logger.error('Error while trying to access Google')
+            return 'Failed to retrieve data from Google.'
+    except Exception as e:
+        return str(e)
+    
+@app.route('/gAnalytics_request', methods=['GET'])
+def gAnalytics_request():
+    try:
+        req2 = requests.get("https://analytics.google.com/analytics/web/#/p407503035/reports/reportinghub?params=_u..nav%3Dmaui")
+        if req2.status_code == 200:
+            return render_template('index.html', cookies=None , status_code=req2.status_code, response_text=req2.text)
+        else:
+            app.logger.error('Error while trying to access GAnalytics')
+            return 'Failed to retrieve data from GAnalytics.'
+    except Exception as e:
+        return str(e)
+    
+    

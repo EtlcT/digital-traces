c3b42e4c2b173aa3656a848a545f11e1897ec3df
EtlcT <agathe.badetti@epfedu.fr>
2023-10-21 17:32:58 +0200

    Adding charts for google trends response; adding comments to app.py

--- app.py
+++ app.py
@@ -9,12 +9,11 @@ from google.analytics.data_v1beta.types import (
     Metric,
     RunReportRequest,
 )
-import pytrends
-import lxml
-import pandas
-from pytrends.request import TrendReq
 
-pytrends = TrendReq(hl='en-US', tz=360)
+from pytrends.request import TrendReq
+import plotly
+import plotly.express as px
+import json
 
 dictConfig({
     'version': 1,
@@ -35,7 +34,7 @@ dictConfig({
 
 # oAuth
 VIEW_ID = "407503035"
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'service.json'
+credentials=os.getenv('GOOGLE_APPLICATION_CREDENTIALS')
 
 
 app = Flask(__name__)
@@ -44,7 +43,7 @@ app = Flask(__name__)
 @app.route("/", methods=['GET','POST'])
 
 def hello_world():
-    
+    ## insert google analytics tag to get insights
     prefix_google = """
 <!-- Google tag (gtag.js) -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-QFMSBHD3XT"></script>
@@ -56,7 +55,8 @@ def hello_world():
   gtag('config', 'G-QFMSBHD3XT');
 </script>
 """
-## to get number of visitors from the beginning :
+## access number of visitors from the creation of our website
+    ##instantiate the client to request google analytics
     client = BetaAnalyticsDataClient()
 
     request_api = RunReportRequest(
@@ -65,13 +65,13 @@ def hello_world():
         Dimension(name="landingPagePlusQueryString")
         ],
         metrics=[
-            Metric(name="newUsers")
+            Metric(name="newUsers") ## specify that we want the number of visitors
         ],
-        date_ranges=[DateRange(start_date="2023-09-01", end_date="today")],
+        date_ranges=[DateRange(start_date="2023-09-01", end_date="today")], # from the publication until today
     )
-    response = client.run_report(request_api)
+    response = client.run_report(request_api) # get response drom google_analytics
 
-    nbvisits = response.rows[0].metric_values[0].value
+    nbvisits = response.rows[0].metric_values[0].value  # access the value
 
     return  prefix_google + render_template('index.html', cookies=None, response_text=None, status_code=None, nbvisits=nbvisits)
 
@@ -81,19 +81,21 @@ def logger():
 
   return '<p>Log page</p>'
 
+## displaying google cookies
 @app.route('/google_request', methods=['GET','POST'])
 def google_request():
+    ## try to access google.com
     try:
         req = requests.get("https://www.google.com/")
-        if req.status_code == 200 :
-            google_cookies = req.cookies.get_dict()
-            return render_template('index.html', cookies=google_cookies, status_code=None, response_text=None, nbvisits=None)
-        else:
+        if req.status_code == 200 : ## if success
+            google_cookies = req.cookies.get_dict() ## access cookies 
+            return render_template('index.html', cookies=google_cookies, status_code=None, response_text=None, nbvisits=None) # add cookies to html
+        else: ## if an error occured
             app.logger.error('Error while trying to access Google')
-            return 'Failed to retrieve data from Google.'
-    except Exception as e:
+    except Exception as e: # inform user
         return str(e)
     
+## accessing google_analytics
 @app.route('/gAnalytics_request', methods=['GET'])
 def gAnalytics_request():
     try:
@@ -102,10 +104,19 @@ def gAnalytics_request():
             return render_template('index.html', cookies=None , status_code=req2.status_code, response_text=req2.text, nbvisits=None)
         else:
             app.logger.error('Error while trying to access GAnalytics')
-            return 'Failed to retrieve data from GAnalytics.'
     except Exception as e:
         return str(e)
 
-@app.route('/gTrends', methods=['GET'])
+## access /gTrends
+@app.route('/gTrends', methods=['GET','POST'])
 def gTrends_request():
-    user_search = request.form()
+    pytrends = TrendReq(hl='en-US', tz=360) # initialize parameter value for trend request
+    user_search = request.form.get('searchTrends').split() # access user request from html form
+    pytrends.build_payload(kw_list=user_search, timeframe='today 3-m') # prepare request
+    result = pytrends.interest_over_time() # request google trends
+    total_interest = result[user_search].sum() # sum along column to get the total interest (for pie chart)
+    fig_line = px.line(result, x=result.index, y=user_search, title='Interest during last 3 months') # creating line plot with plotly express
+    line_graph_json = json.dumps(fig_line, cls=plotly.utils.PlotlyJSONEncoder) # convert plotly plot into json
+    fig_pie = px.pie(result, values=total_interest, names=total_interest.index, title='% of requests during last 3 months') # creating pie chart with plotly express
+    pie_graph_json = json.dumps(fig_pie, cls=plotly.utils.PlotlyJSONEncoder)
+    return render_template('graph.html', line_graph_json=line_graph_json, pie_graph_json=pie_graph_json, user_search=user_search)
